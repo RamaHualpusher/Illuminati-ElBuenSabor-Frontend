95db2109a665c9040781ff4a495baa304d9ea555
matuco47 <matiasroselot@gmail.com>
2023-05-02 23:01:19 -0300

    Botones home

--- src/components/Componentes/BotonesMenu.tsx
+++ src/components/Componentes/BotonesMenu.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useState } from "react";
+import { Form } from "react-bootstrap";
 
 
 const BotonesMenu: FC = () => {
@@ -7,30 +8,67 @@ const BotonesMenu: FC = () => {
     const toggleBotones = () => {
         setBotonesOpen(!BotonesOpen);
     };
-    return (
-        <div className="fok">
-        <ul>
-          <li className="fak">
-            <div className="frame-20">
-              <div className="todos">Todos</div>
-            </div>
-            <div className="frame-19">
-              <div className="ofertas">Ofertas</div>
-            </div>
-            <div className="frame-202">
-              <div className="pizzas">Pizzas</div>
-            </div>
-            <div className="frame-21">
-              <div className="burguer">Burguer</div>
-            </div>
-            <div className="frame-22">
-              <div className="bebidas">Bebidas</div>
-            </div>
-          </li>
-        </ul>
-      </div>
 
+    const [selectedOption, setSelectedOption] = useState("option1");
+
+    const handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSelectedOption(event.target.value);
+    };
+    return (
+        <Form>
+            <Form.Group>
+                <Form.Label>
+                    <div>
+                        <Form.Check
+                            type="radio"
+                            label={<i className="bi bi-0-square-fill">Todos</i>}
+                            name="option"
+                            value="Todos"
+                            checked={selectedOption === "Todos"}
+                            onChange={handleOptionChange}
+                            inline
+                        />
+                        <Form.Check
+                            type="radio"
+                            label={<i className="bi bi-tag-fill">Ofertas</i>}
+                            name="option"
+                            value="Ofertas"
+                            checked={selectedOption === "Ofertas"}
+                            onChange={handleOptionChange}
+                            inline
+                        />
+                        <Form.Check
+                            type="radio"
+                            label={<i className="bi bi-1-square-fill">Pizza</i>}
+                            name="option"
+                            value="Pizzas"
+                            checked={selectedOption === "Pizzas"}
+                            onChange={handleOptionChange}
+                            inline
+                        />
+                        <Form.Check
+                            type="radio"
+                            label={<i className="bi bi-2-square-fill">Burguer</i>}
+                            name="option"
+                            value="Burguer"
+                            checked={selectedOption === "Burguer"}
+                            onChange={handleOptionChange}
+                            inline
+                        />
+                        <Form.Check
+                            type="radio"
+                            label={<i className="bi bi-3-square-fill">Bebidas</i>}
+                            name="option"
+                            value="Bebidas"
+                            checked={selectedOption === "Bebidas"}
+                            onChange={handleOptionChange}
+                            inline
+                        />
+                    </div>
+                </Form.Label>
+            </Form.Group>
+        </Form>
     );
-};
+}
 
 export default BotonesMenu;
