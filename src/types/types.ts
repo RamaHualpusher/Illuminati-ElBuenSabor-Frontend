e18abd54ad27ea83d7dd58cdad9cbf57b3e8d88e
JavierScan <ricardo.scandura@alumnos.frm.utn.edu.ar>
2023-05-04 20:22:25 -0300

    Modifique algo de los botones

--- src/types/types.ts
+++ src/types/types.ts
@@ -8,8 +8,11 @@ export interface Products {
     Descripcion: string;
     Ingredients: OrderIngredient[]
 }
+
 export interface OrderIngredient {
    Ingredient: string;
    Cuantity: string;
    UMedida: string;
 }
+
+//agregar aca las demas interfaces para completar dentro del codigo de front
