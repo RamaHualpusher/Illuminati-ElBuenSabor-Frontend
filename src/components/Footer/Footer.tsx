6e3ec3c0afd3270e9412899f1ae1d004822911e7
Gonzalo <gonzalo.lisanti@alumnos.frm.utn.edu.ar>
2023-05-04 20:18:52 -0300

    Mejora del Footer y agregacion de APIs

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,39 @@
-import React from "react";
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const Footer = () => {
+const Footer: React.FC = () => {
   return (
-    <div className="footer-container bg-secondary">
-      <div className="container">
-        <div className="row justify-content-center align-items-center">
-          <div className="col-md-4 text-center">
-            <h5 className="contact-us">Contactanos</h5>
-            <div className="footer-contact-item">
-              <i className="bi bi-geo-alt icon"></i>
-              <span>Mendoza-Argentina</span>
-            </div>
-            <div className="footer-contact-item">
-              <i className="bi bi-whatsapp icon"></i>
-              <span>+54-9-2616321232</span>
-            </div>
-            <div className="footer-contact-item">
-              <i className="bi bi-envelope icon"></i>
-              <span>elbuensaborutn@gmail.com</span>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="arrow-container">
-              <div className="arrow-bottom">
-                <div className="img-logo"></div>
-              </div>
-            </div>
-          </div>
-          
-        </div>
-      </div>
-    </div>
+    <footer className="bg-dark py-5">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={4}>
+            <p className="text-muted mb-0">© {new Date().getFullYear()} El Buen Sabor</p>
+          </Col>
+          <Col md={7}>
+            <ul className="list-unstyled mb-0 d-flex justify-content-end ">
+              <li className="mx-2">
+                <i className="bi bi-whatsapp text-white"></i>
+                <a href={`https://wa.me/5492613695874`} target="_blank" rel="noreferrer" className="footer-link text-white px-1" >
+                  +54 9 261 3695874
+                </a>
+              </li>
+              <li className="mx-2">
+                <i className="bi bi-geo-alt text-white"></i>
+                <span className="footer-text text-white px-1">Mendoza-Argentina</span>
+              </li>
+              <li className="mx-2">
+                <i className="bi bi-envelope text-white"></i>
+                <a href="mailto:elbuensaborutin@gmail.com" className="footer-link text-white px-1">
+                  elbuensaborutin@gmail.com
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
 export default Footer;
+
