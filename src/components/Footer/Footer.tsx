aa830faed078a4485a7abbd0b46d70cf5cc3cf1a
matuco47 <matiasroselot@gmail.com>
2023-05-04 15:21:55 -0300

    datos footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="footer-container bg-secondary">
       <div className="container">
-        <div className="row">
-          <div className="col-md-4">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-4 text-center">
             <h5 className="contact-us">Contactanos</h5>
             <div className="footer-contact-item">
               <i className="bi bi-geo-alt icon"></i>
@@ -13,11 +13,11 @@ const Footer = () => {
             </div>
             <div className="footer-contact-item">
               <i className="bi bi-whatsapp icon"></i>
-              <span>+54-9-2616172242</span>
+              <span>+54-9-2616321232</span>
             </div>
             <div className="footer-contact-item">
               <i className="bi bi-envelope icon"></i>
-              <span>ElBuenSaborMendoza@gmail.com</span>
+              <span>elbuensaborutn@gmail.com</span>
             </div>
           </div>
           <div className="col-md-4">
@@ -27,16 +27,7 @@ const Footer = () => {
               </div>
             </div>
           </div>
-          <div className="col-md-4">
-            <h5 className="payment-methods">Métodos de pago</h5>
-            <div className="footer-payment-item">
-              <i className="bi bi-credit-card-2-front icon"></i>
-              <i className="bi bi-credit-card-2-front icon"></i>
-              <i className="bi bi-paypal icon"></i>
-              <i className="bi bi-credit-card-2-front icon"></i>
-              <i className="bi bi-apple icon"></i>
-            </div>
-          </div>
+          
         </div>
       </div>
     </div>
