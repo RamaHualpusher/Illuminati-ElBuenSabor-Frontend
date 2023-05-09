bf09735c2be7d804df83072380b7366836c38d33
Gonzalo <gonzalo.lisanti@alumnos.frm.utn.edu.ar>
2023-05-09 16:50:41 -0300

    Cambio de nombre de componente

---
+++ src/components/AdminPage/PanelAdmin.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+interface NavbarAdminProps {
+  title: string;
+}
+
+const PanelAdmin: React.FC<NavbarAdminProps> = ({ title }) => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <h1 className="Title_admin h1 text-white">{title}</h1>
+        <div className="d-flex justify-content-center align-items-center">
+          <button className="btn btn-primary me-2">Perfil</button>
+          <button className="btn btn-primary me-2">Empleados</button>
+          <button className="btn btn-primary me-2">Usuarios</button>
+          <button className="btn btn-primary me-2">Facturas</button>
+          <button className="btn btn-primary me-2">Stock</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default PanelAdmin;
--- src/screens/Admin.tsx
+++ src/screens/Admin.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AdminPage from "../components/AdminPage/AdminPage";
-import NavbarAdmin from "../components/AdminPage/NavbarAdmin";
+import PanelAdmin from "../components/AdminPage/PanelAdmin";
 import NavBar from "../components/NavBar/Navbar";
 
 const Admin = () => {
     return (
         <div>
             <NavBar/>
-            <NavbarAdmin title={"Admin"}/>
+            <PanelAdmin title={"Admin"}/>
             <AdminPage />
         </div>
     );
