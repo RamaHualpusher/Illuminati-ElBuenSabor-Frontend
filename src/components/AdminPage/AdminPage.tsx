18306afd7107bba3eac6ddbaecbe583ef5ad9f52
matuco47 <matiasroselot@gmail.com>
2023-05-08 21:14:54 -0300

    Creacion pag admin

--- src/components/AdminPage/AdminPage.tsx
+++ src/components/AdminPage/AdminPage.tsx
@@ -1,1 +1,25 @@
-export {};
+import React from "react";
+import NavbarEmpleado from "./NavbarEmpleado";
+import NavBar from "../NavBar/Navbar";
+import OpcionesEmpleados from "./OpcionesEmpleados";
+
+export default function AdminPage(){
+
+
+
+    return(
+        <div>
+            <NavBar/>
+            <NavbarEmpleado/>
+            <h1 className="display-1">Admin Page</h1>
+            <OpcionesEmpleados/>
+
+
+            
+
+
+
+        </div>
+    )
+}
+
--- src/routers/UserRouter.tsx
+++ src/routers/UserRouter.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-
-
-import NavbarEmpleado from "../components/Componentes/NavbarEmpleado";
-
+import NavbarEmpleado from "../components/AdminPage/NavbarEmpleado";
 import NavBar from "../components/NavBar/Navbar";
-
 import Landing from "../components/Landing/Landing";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import AdminPage from "../components/AdminPage/AdminPage";
 
 const UserRouter = () => {
   return (
@@ -19,6 +17,7 @@ const UserRouter = () => {
       </Routes>
       <div className="d-flex justify-content-center mb-3">
         <div className="btn-group">
+          <Button  onClick={() =>AdminPage} className="btn btn-success my-3">Pagina Admin</Button>
           <Link to="/cocina/ingredientes" className="btn btn-success my-3">Ingredientes</Link>
           <Link to="/cocina/productos" className="btn btn-success my-3">Productos</Link>
           <Link to="/cocina/rubros/ingredientes" className="btn btn-success my-3">Rubros Ingredientes</Link>
