426a6111435cb9196f1f3ed88983e006017bda46
Gonzalo <gonzalo.lisanti@alumnos.frm.utn.edu.ar>
2023-05-10 16:36:31 -0300

    Implementacion de Spinner

---
+++ src/components/Spinner/Spinner.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Spinner: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+      <div className="text-center">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
+
