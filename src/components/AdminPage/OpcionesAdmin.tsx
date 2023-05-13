a1b41ebb2d98ecf2acb916c50bcfb846d2a0e186
Gonzalo <gonzalo.lisanti@alumnos.frm.utn.edu.ar>
2023-05-12 21:01:19 -0300

    Cambios en las opciones del admin a la izquierda

--- src/components/AdminPage/OpcionesAdmin.tsx
+++ src/components/AdminPage/OpcionesAdmin.tsx
@@ -3,120 +3,227 @@ import { Link } from "react-router-dom";
 
 export default function OpcionesAdmin() {
     return (
-        <div className="d-flex flex-column bd-highligt mb-3 bg-secondary">
-            <div className="row justify-content-center">
-                <div className="col-md-3">
-                <Link to="/admin/employee" className="card-link" style={{ color: "black", textDecoration: "none" }}>
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Empleados</h5>
-                        </div>
-                    </div>
-                    </Link>
-                </div>
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Clientes</h5>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Ranking Clientes</h5>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="row justify-content-center">
-                <div className="col-md-3" >
-                    <Link to="/cocina/productos" className="card-link" style={{ color: "black", textDecoration: "none" }}>
-                        <div className="card my-3" style={{ border: "1px solid black" }}>
-                            <div className="card-body">
-                                <h5>Productos</h5>
+        <div className="d-flex flex-column bd-highlight mb-3 bg-secondary p-3"
+            style={{ width: "250px", height: "100vh", overflowY: "scroll" }}>
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <Link
+                        to="/admin/employee"
+                        className="card-link"
+                        style={{ color: "black", textDecoration: "none" }}
+                    >
+                        <div
+                            className="card"
+                            style={{ border: "1px solid black", width: "100%" }}
+                        >
+                            <div className="card-body p-2">
+                                <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                    Empleados
+                                </h6>
                             </div>
                         </div>
                     </Link>
                 </div>
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Ranking Productos</h5>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Ranking Ventas</h5>
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Clientes
+                            </h6>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="row justify-content-center">
-                <div className="col-md-3">
-                    <Link to="/cocina/ingredientes" className="card-link" style={{ color: "black", textDecoration: "none" }}>
-                        <div className="card my-3" style={{ border: "1px solid black" }}>
-                            <div className="card-body">
-                                <h5>Ingredientes</h5>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Ranking Clientes
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Ranking Productos
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Ranking Ventas
+                            </h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <Link
+                        to="/cocina/productos"
+                        className="card-link"
+                        style={{ color: "black", textDecoration: "none" }}
+                    >
+                        <div
+                            className="card"
+                            style={{ border: "1px solid black", width: "100%" }}
+                        >
+                            <div className="card-body p-2">
+                                <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                    Productos
+                                </h6>
                             </div>
                         </div>
                     </Link>
                 </div>
-                <div className="col-md-3">
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
                     <Link to="/cocina/rubros/ingredientes" className="card-link" style={{ color: "black", textDecoration: "none" }}>
-                        <div className="card my-3" style={{ border: "1px solid black" }}>
-                            <div className="card-body">
-                                <h5>Rubros Ingredientes</h5>
+                        <div
+                            className="card"
+                            style={{ border: "1px solid black", width: "100%" }}
+                        >
+                            <div className="card-body p-2">
+                                <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                    Ingredientes
+                                </h6>
                             </div>
                         </div>
                     </Link>
                 </div>
-                <div className="col-md-3">
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <Link to="/cocina/rubros/ingredientes" className="card-link" style={{ color: "black", textDecoration: "none" }}>
+                        <div
+                            className="card"
+                            style={{ border: "1px solid black", width: "100%" }}
+                        >
+                            <div className="card-body p-2">
+                                <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                    Rubro Ingredientes
+                                </h6>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
                     <Link to="/cocina/rubros/productos" className="card-link" style={{ color: "black", textDecoration: "none" }}>
-                        <div className="card my-3" style={{ border: "1px solid black" }}>
-                            <div className="card-body">
-                                <h5>Rubros Productos</h5>
+                        <div
+                            className="card"
+                            style={{ border: "1px solid black", width: "100%" }}
+                        >
+                            <div className="card-body p-2">
+                                <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                    Rubro Productos
+                                </h6>
                             </div>
                         </div>
                     </Link>
                 </div>
             </div>
-            <div className="row justify-content-center">
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Categorias</h5>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Categorias
+                            </h6>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Usuarios</h5>
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Usuarios
+                            </h6>
                         </div>
                     </div>
                 </div>
-                <div className="col-md-3">
+            </div>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
                     <Link to="/admin/facturas" className="card-link" style={{ color: "black", textDecoration: "none" }}>
-                        <div className="card my-3" style={{ border: "1px solid black" }}>
-                            <div className="card-body">
-                                <h5 className="card-title" style={{ color: "black" }}>Facturas</h5>
+                        <div
+                            className="card"
+                            style={{ border: "1px solid black", width: "100%" }}
+                        >
+                            <div className="card-body p-2">
+                                <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                    Facturas
+                                </h6>
                             </div>
                         </div>
                     </Link>
                 </div>
             </div>
-            <div className="row justify-content-center">
-                <div className="col-md-3">
-                    <div className="card my-3" style={{ border: "1px solid black" }}>
-                        <div className="card-body">
-                            <h5 className="card-title" style={{ color: "black" }}>Stock</h5>
+
+            <div className="row justify-content-start mb-3">
+                <div className="col">
+                    <div
+                        className="card"
+                        style={{ border: "1px solid black", width: "100%" }}
+                    >
+                        <div className="card-body p-2">
+                            <h6 className="card-title mb-0" style={{ color: "black" }}>
+                                Stock
+                            </h6>
                         </div>
                     </div>
                 </div>
             </div>
+
+
+
+
+
         </div>
     )
 }
