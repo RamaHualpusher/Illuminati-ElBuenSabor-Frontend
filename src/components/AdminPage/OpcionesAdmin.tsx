53908356e2d73ffe43a91b3961f19ab27e9b747b
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-05-22 22:15:21 -0300

    OpcionesAdmin

--- src/components/AdminPage/OpcionesAdmin.tsx
+++ src/components/AdminPage/OpcionesAdmin.tsx
@@ -14,191 +14,51 @@ export default function OpcionesAdmin() {
         setSelectedOption(event.currentTarget.value);
     };
 
-    return (
+    const options = [
+        { name: 'Empleados', value: 'employee' },
+        { name: 'Clientes', value: 'clientes' },
+        { name: 'Ranking Clientes', value: 'rankingClientes' },
+        { name: 'Ranking Productos', value: 'rankingProductos' },
+        { name: 'Ranking Ventas', value: 'rankingVentas' },
+        { name: 'Productos', value: 'productos' },
+        { name: 'Ingredientes', value: 'ingredientes' },
+        { name: 'Categorías', value: 'categorias' },
+        { name: 'Usuarios', value: 'usuarios' },
+        { name: 'Facturas', value: 'facturas' },
+      ];
+      
+      return (
         <div className="d-flex flex-row bd-highlight mb-3">
-            <div className="d-flex flex-column bd-highlight mb-3 bg-secondary p-3"
-                style={{ width: "250px", height: "100vh", overflowY: "scroll" }}>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="employee"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}>
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Empleados
-                                </h6>
-                            </div>
-                        </button>
+          <div className="d-flex flex-column bd-highlight mb-3 bg-secondary p-3"
+            style={{ width: "250px", height: "100vh", overflowY: "scroll" }}>
+            {options.map(option => (
+              <div className="row justify-content-start mb-3" key={option.value}>
+                <div className="col">
+                  <button
+                    value={option.value}
+                    onClick={handleOptionChange}
+                    className="btn btn-light btn-lg"
+                    style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
+                  >
+                    <div className="card-body p-1">
+                      <h6 className="card-title mb-0" style={{ color: "black" }}>
+                        {option.name}
+                      </h6>
                     </div>
+                  </button>
                 </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="clientes"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Clientes
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="employee"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Ranking Clientes
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="employee"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Ranking Productos
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="employee"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Ranking Ventas
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="productos"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Productos
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="ingredientes"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Ingredientes
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="employee"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Categorías
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="employee"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Usuarios
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-
-                <div className="row justify-content-start mb-3">
-                    <div className="col">
-                        <button
-                            value="facturas"
-                            onClick={handleOptionChange}
-                            className="btn btn-light btn-lg"
-                            style={{ color: "black", textDecoration: "none", width: "100%", border: "1px solid black" }}
-                        >
-                            <div className="card-body p-1">
-                                <h6 className="card-title mb-0" style={{ color: "black" }}>
-                                    Facturas
-                                </h6>
-                            </div>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            
-            <div>
-                {selectedOption === "employee" && <Employee />}
-                {selectedOption === "productos" && <ProductosTable />}
-                {selectedOption === "ingredientes" && <IngredientesTable />}
-                {selectedOption === "facturas" && <Factura />}
-                {selectedOption === "clientes" && <Cliente />}
-            </div>
-
+              </div>
+            ))}
+          </div>
+      
+          <div>
+            {selectedOption === "employee" && <Employee />}
+            {selectedOption === "productos" && <ProductosTable />}
+            {selectedOption === "ingredientes" && <IngredientesTable />}
+            {selectedOption === "facturas" && <Factura />}
+            {selectedOption === "clientes" && <Cliente />}
+          </div>
+      
         </div>
-    )
+      )
 }
