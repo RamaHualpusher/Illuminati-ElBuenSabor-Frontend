476a9ad80358c039c2169d7e2b8ae8cf671e7131
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2023-06-03 20:34:48 -0300

    BackButtom

--- src/components/BackButtom/backButtom.tsx
+++ src/components/BackButtom/backButtom.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-export function BackButton() {
+function BackButton() {
   const navigate = useNavigate();
 
   const goBack = () => {
@@ -14,6 +14,8 @@ export function BackButton() {
     </button>
   );
 }
+
+export default BackButton;
 
 
   
