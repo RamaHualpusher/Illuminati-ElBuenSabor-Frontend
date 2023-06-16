e4ab6ce0f294fe2b0cd52957199f43e3f01ca81f
matuco47 <matiasroselot@gmail.com>
2023-06-15 22:14:01 -0300

    Link a Factura

--- src/components/FuncionRequest/FuncionRequest.tsx
+++ src/components/FuncionRequest/FuncionRequest.tsx
@@ -18,4 +18,31 @@ export const handleRequest = async (method: string, endpoint: string, body?: obj
       console.log(error);
     }
   };
+
+
+  export const handleRequestSingle = async (method: string, endpoint: string, id?: string, body?: object) => {
+    try {
+      let url = endpoint;
+      if (id) {
+        url += `/${id}`;
+      }
+  
+      const response = await fetch(url, {
+        method: method,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        throw new Error('Error al realizar la solicitud');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   
--- src/components/Pedidos/PedidoList.tsx
+++ src/components/Pedidos/PedidoList.tsx
@@ -5,13 +5,15 @@ import PedidoCard from "./PedidoCard";
 interface PedidoListProps {
     pedidos: Pedido[];
     cambiarEstadoPedido: (nuevoEstado: string) => void;
+    btnTicket:boolean;
+    phat:string;
 }
 
-const PedidoList: React.FC<PedidoListProps> = ({ pedidos, cambiarEstadoPedido }) => {
+const PedidoList: React.FC<PedidoListProps> = ({ pedidos, cambiarEstadoPedido, btnTicket, phat }) => {
     return (
         <div className="card-container container-sm">
             {pedidos.map((pedido) => (
-                <PedidoCard key={pedido.idPedido} pedido={pedido} cambiarEstadoPedido={cambiarEstadoPedido} />
+                <PedidoCard key={pedido.idPedido} pedido={pedido} cambiarEstadoPedido={cambiarEstadoPedido} btnTikect={btnTicket} phat={phat}/>
             ))}
         </div>
     );
