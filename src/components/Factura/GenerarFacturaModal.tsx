7a8346c5e7fa6b7af57afe3ae64847d0289b2e19
JavierScan <ricardo.scandura@alumnos.frm.utn.edu.ar>
2023-06-22 01:09:13 -0300

    agregue backbutton a generar factura

--- src/components/Factura/GenerarFacturaModal.tsx
+++ src/components/Factura/GenerarFacturaModal.tsx
@@ -8,6 +8,8 @@ import { Usuario } from "../../interface/Usuario";
 import { Domicilio } from "../../interface/Domicilio";
 import { useParams } from "react-router-dom";
 import { handleRequest } from "../FuncionRequest/FuncionRequest";
+import { Navbar } from "react-bootstrap";
+import BackButton from "../BackButtom/backButtom";
 
 const GenerarFacturaModal: React.FC = () => {
   //const { show, handleClose, handleFacturaAdd, factura } = props;
@@ -25,9 +27,9 @@ const GenerarFacturaModal: React.FC = () => {
           const pedidos: Pedido[] = responseData;
           setPedidos(pedidos?.find((pedido: Pedido) => pedido.idPedido === parseInt(id)))
           if (pedido)
-          setUsuario(pedido.Usuario);
-          if(usuario)
-          setDomicilio(usuario.Domicilio);
+            setUsuario(pedido.Usuario);
+          if (usuario)
+            setDomicilio(usuario.Domicilio);
           setDetallePedidos(pedido?.DetallePedido || []);
         }
       } catch (error) {
@@ -39,77 +41,83 @@ const GenerarFacturaModal: React.FC = () => {
 
 
   return (
-    <center>
-      <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', width: '400px' }}>
-        <img src="/assets/img/logo-grupo-illuminati.jpg" alt="Logo de la empresa" width={100} />
-        <div style={{ textAlign: 'left', marginBottom: '10px' }}>
-          El Buen Sabor
-          <br />
-          Dirección: Aristides villanueva 356, Ciudad
-          <br />
-          CUIT: 12-5541252-8
-        </div>
-        <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', textAlign: 'left', marginBottom: '10px' }}>
-          <h2>DETALLES DEL PEDIDO</h2>
-          <p>Número de Pedido: {pedido?.numeroPedido}</p>
-          <p>Fecha: {pedido?.fechaPedido.toString()}</p>
-          <p>Forma de Pago: {pedido?.esEfectivo ? 'Efectivo' : 'Mercado Pago'}</p>
-        </div>
-        <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', marginBottom: '10px' }}>
-          <table>
-            <thead>
-              <tr>
-                <th>Cantidad</th>
-                <th>Detalle Producto</th>
-                <th>Precio Unit.</th>
-              </tr>
-            </thead>
-            <tbody>
-              {detallePedidos?.map((detalle) => (
-                <tr key={detalle?.idDetallePedido}>
-                  <td>{detalle?.cantidad}</td>
-                  <td>{detalle?.Producto.nombre}</td>
-                  <td>{detalle?.Producto.precio}</td>
-                </tr>
-              ))}
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan={3}>Total: {pedido?.totalPedido}</td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-        <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', textAlign: 'right', marginBottom: '10px' }}>
-          Tipo de Pago: {pedido?.esEfectivo ? 'Efectivo' : 'Mercado Pago'}
-          <br />
-          Descuento: {/* Agrega el descuento */}
-          <br />
-          Envío: {pedido?.esDelivery ? 'Envío domicilio' : 'Retiro local'}
-          <br />
-          Total a pagar: {pedido?.totalPedido}
-        </div>
-        <div style={{ textAlign: 'left', marginBottom: '10px' }}>
-          <h2>Envío</h2>
-          <p>
-            Dirección: {usuario?.Domicilio.calle} {usuario?.Domicilio.numero},{' '}
-            {usuario?.Domicilio.localidad}
-          </p>
-        </div>
-        <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', textAlign: 'justify', marginBottom: '10px' }}>
-          <p>
-            Muchas gracias {usuario?.nombre} {usuario?.apellido} por comprar en
-            <br />
-            El Buen Sabor
-          </p>
-        </div>
-        <div>
-          <button className="btn btn-primary">Descarga</button>
-          <button className="btn btn-primary">Nota de Crédito</button>
-          <button className="btn btn-primary">Compartir</button>
-        </div>
+    <>
+      <div style={{ display: 'flex', justifyContent: 'flex-start' }}>
+        <BackButton />
       </div>
-    </center>
+      <center>
+        <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', width: '400px' }}>
+          <img src="/assets/img/logo-grupo-illuminati.jpg" alt="Logo de la empresa" width={100} />
+          <div style={{ textAlign: 'left', marginBottom: '10px' }}>
+            El Buen Sabor
+            <br />
+            Dirección: Aristides villanueva 356, Ciudad
+            <br />
+            CUIT: 12-5541252-8
+          </div>
+          <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', textAlign: 'left', marginBottom: '10px' }}>
+            <h2>DETALLES DEL PEDIDO</h2>
+            <p>Número de Pedido: {pedido?.numeroPedido}</p>
+            <p>Fecha: {pedido?.fechaPedido.toString()}</p>
+            <p>Forma de Pago: {pedido?.esEfectivo ? 'Efectivo' : 'Mercado Pago'}</p>
+          </div>
+          <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', marginBottom: '10px' }}>
+            <table>
+              <thead>
+                <tr>
+                  <th>Cantidad</th>
+                  <th>Detalle Producto</th>
+                  <th>Precio Unit.</th>
+                </tr>
+              </thead>
+              <tbody>
+                {detallePedidos?.map((detalle) => (
+                  <tr key={detalle?.idDetallePedido}>
+                    <td>{detalle?.cantidad}</td>
+                    <td>{detalle?.Producto.nombre}</td>
+                    <td>{detalle?.Producto.precio}</td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={3}>Total: {pedido?.totalPedido}</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+          <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', textAlign: 'right', marginBottom: '10px' }}>
+            Tipo de Pago: {pedido?.esEfectivo ? 'Efectivo' : 'Mercado Pago'}
+            <br />
+            Descuento: {/* Agrega el descuento */}
+            <br />
+            Envío: {pedido?.esDelivery ? 'Envío domicilio' : 'Retiro local'}
+            <br />
+            Total a pagar: {pedido?.totalPedido}
+          </div>
+          <div style={{ textAlign: 'left', marginBottom: '10px' }}>
+            <h2>Envío</h2>
+            <p>
+              Dirección: {usuario?.Domicilio.calle} {usuario?.Domicilio.numero},{' '}
+              {usuario?.Domicilio.localidad}
+            </p>
+          </div>
+          <div style={{ border: '1px solid black', borderRadius: '10px', padding: '10px', textAlign: 'justify', marginBottom: '10px' }}>
+            <p>
+              Muchas gracias {usuario?.nombre} {usuario?.apellido} por comprar en
+              <br />
+              El Buen Sabor
+            </p>
+          </div>
+          <div>
+            <button className="btn btn-primary">Descarga</button>
+            <button className="btn btn-primary">Nota de Crédito</button>
+            <button className="btn btn-primary">Compartir</button>
+          </div>
+        </div>
+      </center>
+    </>
+
   );
 };
 
