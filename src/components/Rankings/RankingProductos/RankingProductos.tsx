f456954128829bc99d4615c716d458e487ab836d
JavierScan <ricardo.scandura@alumnos.frm.utn.edu.ar>
2023-09-05 11:17:35 -0300

    rankingProductos, falta excl y verifica de fecha

--- src/components/Rankings/RankingProductos/RankingProductos.tsx
+++ src/components/Rankings/RankingProductos/RankingProductos.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import { Producto } from '../../../interface/Producto';
 import GenericTable from "../../GenericTable/GenericTable";
 import { Column } from "../../../interface/CamposTablaGenerica";
 import { Pedido } from "../../../interface/Pedido";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const RankingProductos = () => {
     const [productos, setProductos] = useState<Producto[]>([]);
     const [addModalShow, setAddModalShow] = useState(false);
     const [pedidos, setPedidos] = useState<Pedido[]>([]);
+    const [searchText, setSearchText] = useState<string>('');  //agregue este, pero entiendo que podria utilizarse el search de genericTable
+    const [startDate, setStartDate] = useState<Date | null>(new Date());
+    const [endDate, setEndDate] = useState<Date | null>(new Date());
 
     useEffect(() => {
         const API_URL = "assets/data/productosLanding.json";
@@ -18,33 +23,33 @@ const RankingProductos = () => {
             fetch(API_URL).then(response => response.json()),
             fetch(API_URL_pedido).then(response => response.json())
         ])
-        .then(([productosData, pedidosData]) => {
-            setProductos(productosData);
-            setPedidos(pedidosData);
-        })
-        .catch(error => console.log(error));
+            .then(([productosData, pedidosData]) => {
+                setProductos(productosData);
+                setPedidos(pedidosData);
+            })
+            .catch(error => console.log(error));
     }, []);
 
     const columns: Column<Producto>[] = [
         { title: "ID", field: "idProducto", width: 1 },
         {
-            title: "Producto", field: "imagen", width: 2,
-            render: (rowData) => <img src={rowData.imagen} alt="Producto" style={{ width: "150px", height: "100px" }} />
+            title: "Producto", field: "imagen", width: 1,
+            render: (rowData) => <img src={rowData.imagen} alt="Producto" style={{ width: "120px", height: "100px" }} />
         },
         { title: "Nombre", field: "nombre", width: 4 },
         {
             title: "Ventas por producto", field: "stockActual", width: 3,
             render: (rowData) =>
-                <div>{calculateCantidadVendido(rowData.idProducto)}</div>
+                <div>{calculateCantidadVendido(rowData.idProducto, false)}</div>
         },
     ];
 
-    const calculateCantidadVendido = (productoId: number) => {
+    const calculateCantidadVendido = (productoId: number, esBebida: boolean) => {
         return pedidos.reduce((totalCantidad, pedido) => {
             const cantidadProductoEnPedido = pedido.DetallePedido.reduce((cantidad, detalle) => {
                 if (detalle.Productos && detalle.Productos.length > 0) {
                     const cantidadProducto = detalle.Productos.reduce((cantidadProductoDetalle, producto) => {
-                        if (producto.idProducto === productoId) {
+                        if (producto.idProducto === productoId && producto.esBebida === esBebida) {
                             cantidadProductoDetalle += detalle.cantidad;
                         }
                         return cantidadProductoDetalle;
@@ -55,6 +60,43 @@ const RankingProductos = () => {
             }, 0);
             return totalCantidad + cantidadProductoEnPedido;
         }, 0);
+    };
+
+    const handleBuscarClick = () => {
+
+        if (startDate !== null && endDate !== null) {
+            const pedidosFiltrados = pedidos.filter(pedido => {
+                const fechaPedido = new Date(pedido.fechaPedido);
+                console.log('Fecha Pedido:', fechaPedido);
+                console.log('StartDate:', startDate);
+                console.log('EndDate:', endDate);
+                return fechaPedido >= startDate && fechaPedido <= endDate;
+            });
+
+            const ventasPorProducto = productos.map(producto => {
+                const ventas = pedidosFiltrados.reduce((total, pedido) => {
+                    const detallePedido = pedido.DetallePedido.find(detalle => Array.isArray(detalle.Productos) && detalle.Productos.some(p => p.idProducto === producto.idProducto));
+                    if (detallePedido) {
+                        return total + detallePedido.cantidad;
+                    }
+                    return total;
+                }, 0);
+                return { ...producto, ventas };
+            });
+
+            // Filtrar los productos por el texto ingresado
+            const productosFiltrados = ventasPorProducto.filter(producto =>
+                producto.nombre.toLowerCase().includes(searchText.toLowerCase())
+            );
+
+            // Ordenar la lista de productos por ventas
+            productosFiltrados.sort((a, b) => b.ventas - a.ventas);
+
+            // Actualizar el estado con los productos filtrados y ordenados
+            setProductos(productosFiltrados);
+        } else {
+            alert("Por favor, seleccione ambas fechas antes de realizar la búsqueda.");
+        }
     };
 
     const handleAddModalOpen = () => {
@@ -70,17 +112,93 @@ const RankingProductos = () => {
         imagen: producto.imagen,
         nombre: producto.nombre,
         denominacion: producto.denominacion,
-        stockActual: calculateCantidadVendido(producto.idProducto), // Cambiamos esto por la cantidad vendida
+        stockActual: calculateCantidadVendido(producto.idProducto, false),
     }));
+
+    const productosBebida = productos
+        .filter(producto => producto.esBebida)
+        .map(producto => ({
+            ...producto,
+            ventasNoBebida: calculateCantidadVendido(producto.idProducto, false),
+            ventasBebida: calculateCantidadVendido(producto.idProducto, true)
+        }));
+
+    const productosNoBebida = productos
+        .filter(producto => !producto.esBebida)
+        .map(producto => ({
+            ...producto,
+            ventasNoBebida: calculateCantidadVendido(producto.idProducto, false),
+            ventasBebida: calculateCantidadVendido(producto.idProducto, true)
+        }));
+
+    const mergedProducts = [...productosBebida, ...productosNoBebida];
 
     return (
         <div>
             <Container fluid>
                 <Row className="mt-3">
                     <Col>
+                        <Form>
+                            <Form>
+                                <Col>
+                                    <Row>
+                                        <Col>
+                                            <Form.Group>
+                                                <Form.Label>Fecha inicio búsqueda</Form.Label>
+                                                <Col>
+                                                    <DatePicker
+                                                        selected={startDate}
+                                                        onChange={(date: Date | null) => setStartDate(date)}
+                                                        dateFormat="yyyy-MM-dd"
+                                                        isClearable
+                                                        className="form-control"
+                                                    />
+                                                </Col>
+                                            </Form.Group>
+                                        </Col>
+                                        <Col>
+                                            <Form.Group>
+                                                <Form.Label>Fecha fin búsqueda</Form.Label>
+                                                <Col>
+                                                    <DatePicker
+                                                        selected={endDate}
+                                                        onChange={(date: Date | null) => setEndDate(date)}
+                                                        dateFormat="yyyy-MM-dd"
+                                                        isClearable
+                                                        className="form-control"
+                                                    />
+                                                </Col>
+                                            </Form.Group>
+                                        </Col>
+                                        <Col>
+                                            <Button variant="primary" style={{ marginTop: "30px" }} onClick={handleBuscarClick}>Buscar</Button>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Form>
+                        </Form>
+                    </Col>
+                </Row>
+                <Row className="mt-3">
+                    <Col md={6}>
+                        <h2>Productos Cocina</h2>
                         <GenericTable<Producto>
                             columns={columns}
-                            data={productos.sort((a, b) => b.stockActual - a.stockActual)}
+                            data={productosNoBebida.sort((a, b) => b.ventasNoBebida - a.ventasNoBebida)}
+                            actions={{
+                                create: true,
+                                update: false,
+                                delete: false,
+                                view: false,
+                            }}
+                            onAdd={handleAddModalOpen}
+                        />
+                    </Col>
+                    <Col md={6}>
+                        <h2>Productos Bebidas</h2>
+                        <GenericTable<Producto>
+                            columns={columns}
+                            data={productosBebida.sort((a, b) => b.ventasBebida - a.ventasBebida)}
                             actions={{
                                 create: true,
                                 update: false,
