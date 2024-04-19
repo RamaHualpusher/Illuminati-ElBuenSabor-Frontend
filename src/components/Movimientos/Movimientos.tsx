88478fd9339beb42b1b40068188e6b9b9d54bca1
JavierScan <ricardo.scandura@alumnos.frm.utn.edu.ar>
2024-04-19 11:06:47 -0300

    Mod Javier

--- src/components/Movimientos/Movimientos.tsx
+++ src/components/Movimientos/Movimientos.tsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Col, Modal } from 'react-bootstrap';
 import GenericTable from '../GenericTable/GenericTable';
 import { IColumn } from '../../interface/ICamposTablaGenerica';
 import { exportTableDataToExcel } from '../../util/exportTableDataToExcel';
 import axios from 'axios';
-import { IDetallePedidoDto } from '../../interface/IDetallePedido';
-import { IProductoDto } from '../../interface/IProducto';
+import { IDetallePedido } from '../../interface/IDetallePedido';
+import { IProducto } from '../../interface/IProducto';
 import { IIngredientes } from '../../interface/IIngredientes';
 import { IPedidoDto } from '../../interface/IPedido';
+import GenerarTicket from '../Ticket/GenerarTicket';
+import NoHayPedidos from '../Page404/NoHayPedidos';
+import { IFactura } from '../../interface/IFactura';
 
 const Movimientos = () => {
-  const [pedidos, setPedidos] = useState<IPedidoDto[]>([]);  
+  const [pedidos, setPedidos] = useState<IPedidoDto[]>([]);
   const API_URL = process.env.REACT_APP_API_URL || "";
-  
+  const [selectedPedido, setSelectedPedido] = useState<IPedidoDto | null>(null);
+  const [showPedidoModal, setShowPedidoModal] = useState<boolean>(false);
+  const [showModal, setShowModal] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -20,16 +26,30 @@ const Movimientos = () => {
         const pedidosData = pedidosResponse.data;
         console.log(pedidosData)
         // Ordenar los pedidos por fecha de pedido de manera descendente
-        pedidosData.sort((a: { fechaPedido: string | number | Date; }, b: { fechaPedido: string | number | Date; }) => 
-          new Date(b.fechaPedido).getTime() - new Date(a.fechaPedido).getTime());
+        pedidosData.sort((a: { fechaPedido: string | number | Date; }, b: { fechaPedido: string | number | Date; }) =>
+          new Date(b.fechaPedido).getDate() - new Date(a.fechaPedido).getDate());
 
-        setPedidos(pedidosResponse.data);        
+        setPedidos(pedidosResponse.data);
       } catch (error) {
         console.error('Error al cargar datos:', error);
-      }    
+      }
     };
     fetchData();
   }, []);
+
+  const openPedidoModal = async (pedido: IPedidoDto) => {
+    setSelectedPedido(pedido);
+    try {
+      setShowPedidoModal(true);
+    } catch (error) {
+      console.error('Error al generar la factura:', error);
+    }
+  };
+
+  // Función para cerrar el modal y actualizar el estado showPedidoModal
+  const handleClosePedidoModal = () => {
+    setShowPedidoModal(false);
+  };
 
   const columns: IColumn<IPedidoDto>[] = [
     { title: "Fecha de Pedido", field: "fechaPedido", width: 2 },
@@ -39,7 +59,6 @@ const Movimientos = () => {
       field: "usuario",
       render: (pedido: IPedidoDto) => {
         const usuario = pedido.usuario;
-        console.log("Usuario desde la carga de columnas: "+usuario);
         return (
           <span>
             {usuario ? `${usuario.apellido} ${usuario.nombre}` : 'Usuario no disponible'}
@@ -47,70 +66,69 @@ const Movimientos = () => {
         );
       },
       width: 2
-    },   
+    },
     {
       title: "Total del Pedido",
       field: "esEfectivo",
-      render: (pedido: IPedidoDto) => {        
+      render: (pedido: IPedidoDto) => {
         return <div>{calcularTotalPedido(pedido)}</div>;
       },
       width: 2
     },
-     {
-       title: "Precio de Costo",
-       field: "fechaPedido",
-       width: 2,
-       render: (pedido: IPedidoDto) => {       
-         return <div>{calcularPrecioCosto(pedido)}</div>;
-       },
-     },
-     {
-       title: "Ganancia Neta",
-       field: "fechaPedido",
-       width: 2,
-       render: (pedido: IPedidoDto) => {        
-         return <div>{calcularGananciaNeta(pedido)}</div>;
-       },
-     },
+    {
+      title: "Precio de Costo",
+      field: "fechaPedido",
+      width: 1,
+      render: (pedido: IPedidoDto) => {
+        return <div>{calcularPrecioCosto(pedido)}</div>;
+      },
+    },
+    {
+      title: "Ganancia Neta",
+      field: "fechaPedido",
+      width: 1,
+      render: (pedido: IPedidoDto) => {
+        return <div>{calcularGananciaNeta(pedido)}</div>;
+      },
+    },   
   ];
-  
 
   const calcularGananciaNeta = (pedido: IPedidoDto) => {
     let gananciaNeta = 0;
-  
+
     if (pedido && pedido.detallesPedidos) {
       const totalPedido = calcularTotalPedido(pedido);
       const precioCosto = calcularPrecioCosto(pedido);
       gananciaNeta = totalPedido - precioCosto;
-    }  
-    
+    }
+
     return gananciaNeta;
   };
 
   const calcularTotalPedido = (pedidos: IPedidoDto) => {
     let totalPedido = 0;
-  
+
     if (!pedidos || !pedidos.detallesPedidos) {
       return totalPedido;
     }
-  
-    pedidos.detallesPedidos.forEach((detalle: IDetallePedidoDto) => {
+
+    pedidos.detallesPedidos.forEach((detalle: IDetallePedido) => {
       if (!detalle || !detalle.producto || !detalle.producto.precio || !detalle.cantidad) {
         return;
       }
-  
+
       totalPedido += detalle.producto.precio * detalle.cantidad;
     });
-  
+
     return totalPedido;
   };
 
   const calcularPrecioCosto = (pedido: IPedidoDto) => {
     let costoTotalIngredientes = 0;
-    
+
     if (pedido && pedido.detallesPedidos) {
-      pedido.detallesPedidos.forEach((detalle: IDetallePedidoDto) => {
-        const producto: IProductoDto = detalle.producto;
+      pedido.detallesPedidos.forEach((detalle: IDetallePedido) => {
+        const producto: IProducto = detalle.producto;
         if (producto && producto.productosIngredientes && producto.productosIngredientes.length > 0) {
           producto.productosIngredientes.forEach((pi) => {
             const ingrediente: IIngredientes = pi.ingrediente;
@@ -120,10 +138,9 @@ const Movimientos = () => {
         }
       });
     }
-    
+
     return costoTotalIngredientes;
   };
-  
 
   const exportDataToExcel = () => {
     const dataToExport = pedidos;
@@ -136,7 +153,7 @@ const Movimientos = () => {
       <Container fluid>
         <Row className="mt-3">
           <Col className="d-flex justify-content-center">
-            
+            {pedidos && pedidos.length > 0 ? (
               <GenericTable<IPedidoDto>
                 data={pedidos}
                 columns={columns}
@@ -144,22 +161,25 @@ const Movimientos = () => {
                   create: false,
                   update: false,
                   delete: false,
-                  view: false,
+                  view: true,
                 }}
                 showDate={true}
+                onView={openPedidoModal}
               />
-            
+            ) : (
+              NoHayPedidos
+            )}
           </Col>
-        </Row>
-        {pedidos && pedidos.length > 0 ? (
-          <Button variant="success" onClick={() => exportDataToExcel()}>Exportar a Excel</Button>
-        ) : (
-          <span></span>
-        )}
+        </Row>        
+          <GenerarTicket
+            pedido={selectedPedido}
+            closeModal={handleClosePedidoModal} // Pasar la función de manejo de estado adicional
+            show={showPedidoModal}            
+          />        
+        <Button variant="success" onClick={() => exportDataToExcel()}>Exportar a Excel</Button>
       </Container>
     </div>
   );
 };
 
 export default Movimientos;
-
