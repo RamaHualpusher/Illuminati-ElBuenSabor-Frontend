7b2b470fe2ff6e57554c6a6e522350fca9a84864
RamaHualpusher <ramiro.hualpa@alumnos.frm.utn.edu.ar>
2024-06-10 22:01:25 -0300

    Redirección con botón Trabajo

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -12,24 +12,32 @@ import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import { CartContext } from "../../context/cart/CartProvider";
 import CartItem from "../CarritoCompras/CartItem";
-import { IProducto } from "../../interface/IProducto";
 import { SearchContext } from "../Buscador/SearchContext";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import CustomDropdown from "./CustomDropdown/CustomDropdown";
 import OpcionesUsuario from "./OpcionesUsuario/OpcionesUsuario";
 import LogoutButtonEmployee from "./LogoutButtonEmployee";
 import LoginButtonEmployee from "./LoginButtonEmployee";
+import { useUser } from "../../context/User/UserContext";
 
 const Navbar: FC = () => {
   const { isAuthenticated } = useAuth0(); // Obtener el estado de autenticación de Auth0
   const [navbarOpen, setNavbarOpen] = useState(false); // Estado para controlar la apertura del menú
-  const { cartItems, removeFromCart, clearCart } = useContext(CartContext); // Obtener elementos del carrito y función para eliminar
+  const { cartItems, clearCart } = useContext(CartContext); // Obtener elementos del carrito y función para eliminar
   const [cartOpen, setCartOpen] = useState(false); // Estado para controlar la apertura del carrito
   const [searchOpen, setSearchOpen] = useState(false); // Estado para controlar la apertura de la búsqueda
   const { setSearchParam } = useContext(SearchContext); // Obtener función para establecer parámetros de búsqueda
   const cartVacio = cartItems.length === 0; // Verificar si el carrito está vacío
+  const { usuarioContext, loading, userExists } = useUser(); // Obtener el contexto del usuario
+  const [isEmployee, setIsEmployee] = useState(!!localStorage.getItem("employeeToken"));
+  const navigate = useNavigate();
+
+  // Obtener el token del empleado desde el localStorage
   const employeeToken = localStorage.getItem("employeeToken");
-  const [isEmployee, setIsEmployee] = useState(false);
+
+  useEffect(() => {
+    setIsEmployee(!!employeeToken);
+  }, [employeeToken]);
 
   const toggleNavbar = () => {
     setNavbarOpen(!navbarOpen);
@@ -49,6 +57,27 @@ const Navbar: FC = () => {
 
   const handleSearch = (searchParam: string) => {
     setSearchParam(searchParam);
+  };
+
+  const handleWorkButtonClick = () => {
+    if (usuarioContext) {
+      switch (usuarioContext.rol.nombreRol) {
+        case "Admin":
+          navigate("/admin");
+          break;
+        case "Cajero":
+          navigate("/cajero");
+          break;
+        case "Cocinero":
+          navigate("/cocinero");
+          break;
+        case "Delivery":
+          navigate("/delivery");
+          break;
+        default:
+          break;
+      }
+    }
   };
 
   return (
@@ -105,74 +134,87 @@ const Navbar: FC = () => {
                 ></i>
               </Button>
             </li>
-            <li className="nav-item">
-              <Dropdown show={cartOpen} onToggle={toggleCart}>
-                <Dropdown.Toggle variant="link" id="dropdown-cart">
-                  <i
-                    className="bi bi-cart text-white"
-                    style={{ fontSize: "2rem", marginRight: "10px" }}
-                  ></i>
-                  {cartItems.length > 0 && (
-                    <span className="badge bg-danger">{cartItems.length}</span>
-                  )}
-                </Dropdown.Toggle>
-                <Dropdown.Menu
-                  className="dropdown-menu-custom bg-light"
-                  style={
-                    cartItems.length === 0
-                      ? { maxHeight: "75vh", width: "250px" }
-                      : { maxHeight: "75vh", width: "500px" }
-                  }
+            {employeeToken && (
+              <li className="nav-item">
+                <Button variant="secondary" 
+                className="btn btn-secondary px-3 py-2" 
+                onClick={handleWorkButtonClick}
+                style={{fontSize:"1.2rem", width:"10rem"}}
                 >
-                  <button
-                    className="btn-close close-btn mx-1"
-                    aria-label="Close"
-                    onClick={() => setCartOpen(false)}
-                  ></button>
-                  <h3 className="dropdown-header p-3 ">
-                    Carrito de compras El Buen Sabor
-                  </h3>
-                  {cartOpen && (
-                    <div
-                      className="overflow-auto mx-2"
-                      style={{ maxHeight: "50vh" }}
-                    >
-                      {cartItems.length > 0 ? (
-                        <ListGroup className="list-group-flush">
-                          {cartItems.map((item) => (
-                            <CartItem key={item.id} item={item} />
-                          ))}
-                        </ListGroup>
-                      ) : (
-                        <Dropdown.Item disabled>
-                          No hay productos en el carrito
-                        </Dropdown.Item>
-                      )}
-                      {!cartVacio && (
-                        <>
-                          <Dropdown.Item>
-                            <Link
-                              to={"/confirmacion-pedido"}
-                              className="btn btn-success text-decoration-none w-100 my-2"
-                            >
-                              Comprar
-                            </Link>
+                  Trabajo
+                </Button>
+              </li>
+            )}
+            {!isEmployee && (
+              <li className="nav-item">
+                <Dropdown show={cartOpen} onToggle={toggleCart}>
+                  <Dropdown.Toggle variant="link" id="dropdown-cart">
+                    <i
+                      className="bi bi-cart text-white"
+                      style={{ fontSize: "2rem", marginRight: "10px" }}
+                    ></i>
+                    {cartItems.length > 0 && (
+                      <span className="badge bg-danger">{cartItems.length}</span>
+                    )}
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu
+                    className="dropdown-menu-custom bg-light"
+                    style={
+                      cartItems.length === 0
+                        ? { maxHeight: "75vh", width: "250px" }
+                        : { maxHeight: "75vh", width: "500px" }
+                    }
+                  >
+                    <button
+                      className="btn-close close-btn mx-1"
+                      aria-label="Close"
+                      onClick={() => setCartOpen(false)}
+                    ></button>
+                    <h3 className="dropdown-header p-3 ">
+                      Carrito de compras El Buen Sabor
+                    </h3>
+                    {cartOpen && (
+                      <div
+                        className="overflow-auto mx-2"
+                        style={{ maxHeight: "50vh" }}
+                      >
+                        {cartItems.length > 0 ? (
+                          <ListGroup className="list-group-flush">
+                            {cartItems.map((item) => (
+                              <CartItem key={item.id} item={item} />
+                            ))}
+                          </ListGroup>
+                        ) : (
+                          <Dropdown.Item disabled>
+                            No hay productos en el carrito
                           </Dropdown.Item>
-                          <Dropdown.Item>
-                            <button
-                              className="btn btn-danger text-decoration-none w-100 my-2"
-                              onClick={clearCart}
-                            >
-                              Limpiar carrito
-                            </button>
-                          </Dropdown.Item>
-                        </>
-                      )}
-                    </div>
-                  )}
-                </Dropdown.Menu>
-              </Dropdown>
-            </li>
+                        )}
+                        {!cartVacio && (
+                          <>
+                            <Dropdown.Item>
+                              <Link
+                                to={"/confirmacion-pedido"}
+                                className="btn btn-success text-decoration-none w-100 my-2"
+                              >
+                                Comprar
+                              </Link>
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                              <button
+                                className="btn btn-danger text-decoration-none w-100 my-2"
+                                onClick={clearCart}
+                              >
+                                Limpiar carrito
+                              </button>
+                            </Dropdown.Item>
+                          </>
+                        )}
+                      </div>
+                    )}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </li>
+            )}
             {isAuthenticated && (
               <li className="nav-item d-flex align-items-center mx-2">
                 <Dropdown>
@@ -183,11 +225,11 @@ const Navbar: FC = () => {
             )}
             {employeeToken && (
               <li className="nav-item d-flex align-items-center mx-2">
-              <Dropdown>
-                <CustomDropdown employeeToken={employeeToken}/>
-                <OpcionesUsuario />
-              </Dropdown>
-            </li>
+                <Dropdown>
+                  <CustomDropdown employeeToken={employeeToken}/>
+                  <OpcionesUsuario />
+                </Dropdown>
+              </li>
             )}
             <li className="nav-item">
               {isAuthenticated ? <LogoutButton /> : null}
